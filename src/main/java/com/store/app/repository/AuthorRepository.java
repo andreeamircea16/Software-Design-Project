a92e7e6ccf1a81f6e4e1eb7c7b9b3366754a106c
andreeamircea16 <andreea.mircea16@gmail.com>
2020-04-06 13:06:34 +0300

    used JpaRepository instead of CrudRepository

--- src/main/java/com/store/app/repository/AuthorRepository.java
+++ src/main/java/com/store/app/repository/AuthorRepository.java
@@ -1,13 +1,13 @@
 package com.store.app.repository;
 
 import com.store.app.entity.Author;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface AuthorRepository extends CrudRepository<Author, UUID> {
+public interface AuthorRepository extends JpaRepository<Author, UUID> {
     List<Author> findAllByOrderByFirstName();
 }
--- src/main/java/com/store/app/repository/CategoryRepository.java
+++ src/main/java/com/store/app/repository/CategoryRepository.java
@@ -1,13 +1,13 @@
 package com.store.app.repository;
 
 import com.store.app.entity.Category;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CategoryRepository extends CrudRepository<Category, UUID> {
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
     List<Category> findAllByOrderByName();
 }
